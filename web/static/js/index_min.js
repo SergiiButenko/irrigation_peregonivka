16d31d43e41d12f23f79edecf2183c617670fc70
sergii <butenko2003@ukr.net>
2018-03-11 14:29:01 +0200

    changed relays

--- web/static/js/index_min.js
+++ web/static/js/index_min.js
@@ -249,6 +249,10 @@ function update_branches_request() {
 function update_branches(json) {
     arr = json['branches']
 
+    for (obj in arr){
+        console.log(obj)
+    }
+    
     for (var i = 0; i <= arr.length; i++) {
         var obj = arr[i];
         console.log(obj)
