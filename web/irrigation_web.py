6890f2de1505e8aeefadaef9dfdf2ce1cd062960
sergii <butenko2003@ukr.net>
2018-03-29 23:04:05 +0300

    big badabum

--- web/irrigation_web.py
+++ web/irrigation_web.py
@@ -767,9 +767,10 @@ def lighting_status():
         for line_id, line in BRANCHES_SETTINGS.items():
             if line['line_type'] == 'lighting' and line['base_url'] is not None:
                 response_status = remote_controller.line_status(line_id=line_id)
+                lines[line_id] = dict(id=line_id, state=int(response_status[line_id]['state']))
             elif line['line_type'] == 'lighting' and line['base_url'] is None:
                 response_status = garden_controller.branch_status()
-            lines[line_id] = dict(id=line_id, state=int(response_status[line_id]['state']))
+                lines[line_id] = dict(id=line_id, state=int(response_status[line_id]['state']))
 
         arr = form_responce_for_branches(lines)
         send_branch_status_message(arr)
