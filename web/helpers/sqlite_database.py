42f6f3fdc157156bb7c1d39fe5862eec0161c733
Sergii Butenko <sergii.butenko@globallogic.com>
2018-03-30 13:23:14 +0300

    changed temp

--- web/helpers/sqlite_database.py
+++ web/helpers/sqlite_database.py
@@ -256,8 +256,8 @@ def get_temperature():
         for key, group in groupby(list_arr, itemgetter(0)):
             grouped[key] = {
             'sensor_id': key,
-            'sensor_name': group[4],
-            'sensor_type': group[5],
+            #'sensor_name': group[4],
+            #'sensor_type': group[5],
             'values': [list(thing) for thing in group]
             }
     return grouped
