4f89ea9ae3afc77957cac505e4f5ef4d012fcf34
Sergii Butenko <sergii.butenko@globallogic.com>
2018-03-30 18:19:39 +0300

    added wifi socket without temp

--- arduino/WIFI_SOCKET_TEMPERATURE/WIFI_SOCKET/WIFI_SOCKET.ino
+++ arduino/WIFI_SOCKET_TEMPERATURE/WIFI_SOCKET/WIFI_SOCKET.ino
@@ -3,11 +3,20 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define DHTPIN D4     // what digital pin the DHT22 is conected to
+#define ONE_WIRE_BUS D1
 #define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
 
 DHT dht(DHTPIN, DHTTYPE);
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature DS18B20(&oneWire);
+char temperatureCString[7];
+char temperatureFString[7];
 
 const char* ssid = "NotebookNet";
 const char* password = "0660101327";
@@ -17,6 +26,7 @@ ESP8266WebServer server(80);
 const int led = 13;
 const int r1 = D8;
 const int r2 = D7;
+const int photocellPin = A0;
 
 void handleRoot() {
   digitalWrite(led, 1);
@@ -41,17 +51,13 @@ void handleNotFound(){
   digitalWrite(led, 0);
 }
 
-void send_status(){
-    int r1_status = digitalRead(r1);
-    int r2_status = digitalRead(r2);
-    server.send(200, "application/json", "{\"1\":" + String(r1_status) + ", \"2\":" + String(r2_status) + "}");
-}
-
 void setup(void){
   Serial.println("setup");
   pinMode(led, OUTPUT);
   pinMode(r1, OUTPUT);
   pinMode(r2, OUTPUT);
+  pinMode(photocellPin, INPUT);
+
   digitalWrite(led, 0);
   digitalWrite(r1, 0);
   digitalWrite(r2, 0);
@@ -111,6 +117,19 @@ void setup(void){
     server.send(200, "application/json", "{\"temp\":" + String(t) + ", \"hum\":" + String(h) + "}");
   });
 
+  server.on("/ground_temperature", [](){
+    delay(2000);
+    getTemperature();
+    server.send(200, "application/json", "{\"temp\":" + String(temperatureCString) + "}");
+  });
+
+  server.on("/light_status", [](){
+    delay(2000);
+    
+    photocellReading = analogRead(photocellPin);
+    server.send(200, "application/json", "{\"light\":" + String(photocellReading) + "}");
+  });
+
   server.onNotFound(handleNotFound);
 
   server.begin();
@@ -121,3 +140,21 @@ void loop(void){
   server.handleClient();
 }
 
+void send_status(){
+    int r1_status = digitalRead(r1);
+    int r2_status = digitalRead(r2);
+    server.send(200, "application/json", "{\"1\":" + String(r1_status) + ", \"2\":" + String(r2_status) + "}");
+}
+
+void getTemperature() {
+  float tempC;
+  float tempF;
+  do {
+    DS18B20.requestTemperatures(); 
+    tempC = DS18B20.getTempCByIndex(0);
+    dtostrf(tempC, 2, 2, temperatureCString);
+    tempF = DS18B20.getTempFByIndex(0);
+    dtostrf(tempF, 3, 2, temperatureFString);
+    delay(100);
+  } while (tempC == 85.0 || tempC == (-127.0));
+}
