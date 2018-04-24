8e47307b5f6f73db329c076a93a521023b752922
Sergii Butenko <sergii.butenko@globallogic.com>
2018-04-24 14:29:33 +0300

    changed certs

--- web/telegram_bot/config.py
+++ web/telegram_bot/config.py
@@ -12,8 +12,8 @@ WEBHOOK_HOST = 'butenko.asuscomm.com'
 WEBHOOK_PORT = 443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = '/var/www/web/telegram_bot/certificate.crt'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = '/var/www/web/telegram_bot/private.key'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = '/var/www/web/telegram_bot/mozz/cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = '/var/www/web/telegram_bot/mozz/privkey.pem'  # Path to the ssl private key
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
