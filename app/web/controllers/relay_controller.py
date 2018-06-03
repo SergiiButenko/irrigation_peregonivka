1efbf44a52e44f30a955186079e5adf8a24bf6c9
sergii <butenko2003@ukr.net>
2018-06-04 00:14:51 +0300

    new folder structure

--- app/web/controllers/relay_controller.py
+++ app/web/controllers/relay_controller.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import os.path as path
 import sys
-sys.path.append("../..")
+two_up =  path.abspath(path.join(__file__ ,"../.."))
+sys.path.append(two_up)
 
 import logging
 import time
