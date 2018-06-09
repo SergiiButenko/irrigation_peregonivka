35207c156159c8f9eada8f43d823a8cf97323899
sergii <butenko2003@ukr.net>
2018-06-09 13:40:33 +0300

    Changed swithcer ino

--- arduino/WIFI_SOCKET_SWITCHER/WIFI_SOCKET_SWITCHER.ino
+++ arduino/WIFI_SOCKET_SWITCHER/WIFI_SOCKET_SWITCHER.ino
@@ -10,7 +10,8 @@ const char* password = "0660101327";
 
 ESP8266WebServer server(80);
 
-const int s1 = D5;
+const int s1 = D4;
+const int l1 = D5;
 const int l2 = D6;
 const int r1 = D7;
 const int r2 = D8;
@@ -19,35 +20,47 @@ int state;
 
 void blink_connected() {
   int l2_status = digitalRead(r2);
+  int l1_status = digitalRead(r1);
+  digitalWrite(l1, 0);
   digitalWrite(l2, 0);
   delay(100);
+  digitalWrite(l1, 1);
   digitalWrite(l2, 1);
   delay(100);
+  digitalWrite(l1, 0);
   digitalWrite(l2, 0);
   delay(100);
-  digitalWrite(l2, 0);
-  delay(100);
+  digitalWrite(l1, 1);
   digitalWrite(l2, 1);
   delay(100);
+  digitalWrite(l1, 0);
   digitalWrite(l2, 0);
   delay(100);
-  digitalWrite(l2, 0);
-  delay(100);
+  digitalWrite(l1, 1);
   digitalWrite(l2, 1);
   delay(100);
+  digitalWrite(l1, 0);
   digitalWrite(l2, 0);
-  
+  delay(100);
+  digitalWrite(l1, 1);
+  digitalWrite(l2, 1);
+
+  digitalWrite(l1, l1_status);
   digitalWrite(l2, l2_status);
 }
 
 void blink_led() {
+  int l1_status = digitalRead(r1);
   int l2_status = digitalRead(r2);
- 
+
+  digitalWrite(l1, 0);
   digitalWrite(l2, 0);
   delay(200);
+  digitalWrite(l1, 1);
   digitalWrite(l2, 1);
   delay(200);
 
+  digitalWrite(l2, l2_status);
   digitalWrite(l2, l2_status);
 }
 
@@ -56,16 +69,16 @@ void handleRoot() {
   blink_led();
 }
 
-void handleNotFound(){
+void handleNotFound() {
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
+  for (uint8_t i = 0; i < server.args(); i++) {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
@@ -73,26 +86,28 @@ void handleNotFound(){
 }
 
 
-void send_status(){
-    int r1_status = digitalRead(r1);
-    int r2_status = digitalRead(r2);
-    server.send(200, "application/json", "{\"1\":" + String(r1_status) + ", \"2\":" + String(r2_status) + "}");
+void send_status() {
+  int r1_status = digitalRead(r1);
+  int r2_status = digitalRead(r2);
+  server.send(200, "application/json", "{\"1\":" + String(r1_status) + ", \"2\":" + String(r2_status) + "}");
 }
 
-void setup(void){
+void setup(void) {
   Serial.println("setup");
   pinMode(s1, INPUT_PULLUP);
   pinMode(r1, OUTPUT);
-  
+  pinMode(l1, OUTPUT);
+
   pinMode(r2, OUTPUT);
   pinMode(l2, OUTPUT);
- 
+
   digitalWrite(r1, 0);
   digitalWrite(r2, 0);
+  digitalWrite(l1, 0);
   digitalWrite(l2, 0);
-  
+
   state = digitalRead(s1);
-  
+
   Serial.begin(115200);
   WiFi.setAutoConnect (true);
   WiFi.mode(WIFI_STA);
@@ -100,13 +115,13 @@ void setup(void){
   Serial.println("");
   Serial.println("done");
   blink_led();
-  
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
-    blink_led();   
+    blink_led();
     Serial.print(".");
   }
-  
+
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
@@ -121,36 +136,38 @@ void setup(void){
 
   server.on("/", handleRoot);
 
-  server.on("/test", [](){
+  server.on("/test", []() {
     server.send(200, "text/plain", "this works as well");
     blink_led();
   });
 
-  server.on("/on", [](){
-    String relay=server.arg("relay");
+  server.on("/on", []() {
+    String relay = server.arg("relay");
     if (relay == "1")  digitalWrite(r1, 1);
     if (relay == "2")  digitalWrite(r2, 1);
     int r1_status = digitalRead(r1);
     int r2_status = digitalRead(r2);
-    
+
+digitalWrite(l1, r1_status);
     digitalWrite(l2, r2_status);
     send_status();
     blink_led();
   });
 
-  server.on("/off", [](){
-    String relay=server.arg("relay");
+  server.on("/off", []() {
+    String relay = server.arg("relay");
     if (relay == "1")  digitalWrite(r1, 0);
     if (relay == "2")  digitalWrite(r2, 0);
     int r1_status = digitalRead(r1);
     int r2_status = digitalRead(r2);
- 
+
+digitalWrite(l1, r1_status);
     digitalWrite(l2, r2_status);
     send_status();
     blink_led();
   });
 
-  server.on("/status", [](){
+  server.on("/status", []() {
     send_status();
     blink_led();
   });
@@ -161,13 +178,13 @@ void setup(void){
   Serial.println("HTTP server started");
 }
 
-void loop(void){
+void loop(void) {
   server.handleClient();
   revert_pin();
 }
 
-void revert_pin(){
-  if (digitalRead(s1) != state){
+void revert_pin() {
+  if (digitalRead(s1) != state) {
     delay(500);
     state = digitalRead(s1);
     Serial.println("STATE CHANGED");
