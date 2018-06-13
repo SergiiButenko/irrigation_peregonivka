0a8d140d33f0261eefe2b1bba76a4ae83b686dff
sergii <butenko2003@ukr.net>
2018-06-14 00:46:46 +0300

    changed backend ip

--- app/rules_handler/rules_handler.py
+++ app/rules_handler/rules_handler.py
@@ -18,6 +18,7 @@ from common.common import *
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 
+requests.packages.urllib3.disable_warnings()
 
 def branch_on(line_id, alert_time):
     """Blablbal."""
@@ -27,7 +28,7 @@ def branch_on(line_id, alert_time):
                 response = requests.get(url=BACKEND_IP + '/activate_branch',
                                         params={"id": line_id, 'time_min': alert_time, 'mode': 'auto'},
                                         timeout=(READ_TIMEOUT, RESP_TIMEOUT),
-                                        cert=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
+                                        verify=False)
                 response.raise_for_status()
                 logging.debug('response {0}'.format(response.text))
 
@@ -61,7 +62,7 @@ def branch_off(line_id):
                 response = requests.get(url=BACKEND_IP + '/deactivate_branch',
                                         params={"id": line_id, 'mode': 'auto'},
                                         timeout=(READ_TIMEOUT, RESP_TIMEOUT),
-                                        cert=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
+                                        verify=False)
                 response.raise_for_status()
                 logging.debug('response {0}'.format(response.text))
 
@@ -161,13 +162,13 @@ def send_to_viber_bot(rule):
             response = requests.post(VIBER_BOT_IP + '/send_message',
                                      json=payload,
                                      timeout=(READ_TIMEOUT, RESP_TIMEOUT),
-                                     cert=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
+                                     verify=False)
         else:
             payload = {'rule_id': id, 'line_id': line_id, 'time': time, 'interval_id': interval_id, 'users': USERS, 'timeout': VIBER_SENT_TIMEOUT, 'user_friendly_name': user_friendly_name}
             response = requests.post(VIBER_BOT_IP + '/notify_users_irrigation_started',
                                      json=payload,
                                      timeout=(READ_TIMEOUT, RESP_TIMEOUT),
-                                     cert=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
+                                     verify=False)
         response.raise_for_status()
     except Exception as e:
         logging.error(e)
