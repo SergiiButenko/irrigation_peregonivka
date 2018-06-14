02b3b3329fdb4230113ed2ad8f5998baad4c4ed4
sergii <butenko2003@ukr.net>
2018-06-14 13:24:08 +0300

    changed script

--- tools/scripts/dev_restart_7542.sh
+++ tools/scripts/dev_restart_7542.sh
@@ -9,10 +9,15 @@ cp -uv * /etc/systemd/system/
 
 systemctl daemon-reload
 
+echo 'systemctl restart irrigation_7542.service'
 systemctl restart irrigation_7542.service
+echo 'systemctl restart rules_handler.service'
 systemctl restart rules_handler.service
+echo 'systemctl restart greenhouse_handler.service'
 systemctl restart greenhouse_handler.service
+echo 'systemctl restart irrigation_data_logger.service'
 systemctl restart irrigation_data_logger.service
+echo 'systemctl restart irrigation_telegram_bot.service'
 systemctl restart irrigation_telegram_bot.service
 
 echo 'HEAD is now '$msg
