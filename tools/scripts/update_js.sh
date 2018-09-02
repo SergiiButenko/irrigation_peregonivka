f5d4f1d8b6bbfed784b74b1675f26d414abf148f
sergii <butenko2003@ukr.net>
2018-09-02 18:23:13 +0300

    added js changer

--- tools/scripts/update_js.sh
+++ tools/scripts/update_js.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-C_TIME=`echo $(($(date +%s%N)/1000000))`
-find /var/www/app/web/templates -type f -print0 | xargs -0 sed -i 's/JS_VERSION/'$C_TIME'/g'
+CR_TIME=`echo $(($(date +%s%N)/1000000))`
+find /var/www/app/web/templates -type f -print0 | xargs -0 sed -i 's/JS_VERSION/'$CR_TIME'/g'
