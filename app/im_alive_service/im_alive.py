767de827e4ace31088c23713daa2e6db23e8ca6f
Sergii Butenko <sergii.butenko@globallogic.com>
2018-09-05 11:46:14 +0300

    added ping service

--- app/im_alive_service/im_alive.py
+++ app/im_alive_service/im_alive.py
@@ -3,23 +3,13 @@
 import logging
 from flask import Flask, jsonify, request
 
-import eventlet
-from flask_socketio import SocketIO
-
-eventlet.monkey_patch()
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
     level=logging.INFO,
 )
 
-logging.getLogger("socketio").setLevel(logging.ERROR)
-logging.getLogger("engineio").setLevel(logging.ERROR)
-
 app = Flask(__name__)
-socketio = SocketIO(
-    app, async_mode="eventlet", engineio_logger=False, message_queue="redis://"
-)
 
 
 @app.route("/im_alive")
