59f9fec2c70d59b52f12852215493b58fdb778a6
Sergii Butenko <sergii.butenko@globallogic.com>
2020-05-23 23:15:24 +0300

    impoert

--- app/common/redis_provider.py
+++ app/common/redis_provider.py
@@ -5,7 +5,7 @@ import redis
 from helpers import *
 from config import *
 
-redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
+redis_db = redis.Redis(host="localhost", port=6379, db=0)
 
 
 def set_next_rule_to_redis(branch_id, data):
